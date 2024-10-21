63732a9d222c74646b74543788a2792af87a74b9
sandeepkumar1209 <59911291+sandeepkumar1209@users.noreply.github.com>
2024-10-21 12:36:05 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,7 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.66"
+      version = ">= 4.7.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
     }
   }
 }
